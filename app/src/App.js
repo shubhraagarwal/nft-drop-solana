b4b17c36b2d40b0b986fd8b89141b4632021e0f2
shubhraagarwal <agarwalshubhra2002@gmail.com>
2022-02-10 09:40:16 +0530

    :sparkles: Connected wallet and NFT, metadata

---
+++ app/src/App.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+// import twitterLogo from "./assets/twitter-logo.svg";
+
+// Constants
+const TWITTER_HANDLE = "_buildspace";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+  const checkifWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana && solana.isPhantom) {
+        console.log("Phantom Wallet found!!");
+
+        // the connect function is given to us by the solana object which will allow us to connect with the user's wallet
+
+        const response = await solana.connect({ onlyIfTrusted: true });
+        console.log("Public KEY", response.publicKey.toString());
+        setWalletAddress(response.publicKey.toString());
+      } else {
+        alert("No wallet found. Please connect your wallet.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkifWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🍭 Candy Shop</p>
+          <p className="sub-text">I take you to the candy shop</p>
+          <p className="sub-text">I let you lick the lollipop</p>
+          {!walletAddress && renderNotConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
